bfa8b9a2fad87672cd0cc2ea9d48aeb86222c8d0
Lyzunenko Artem <lyzunenko.artem@vu.cdu.edu.ua>
2020-12-21 23:34:49 +0200

    .NET Framework

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ namespace MTZKIT
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("MTZKIT");
             Console.Readkey();
         }
     }
